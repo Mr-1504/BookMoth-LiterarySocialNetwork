b9ac4e91c1b45d6c48d584cfcdf6192956ca2c71
Mr-1504 <truongvan.minh1504@gmail.com>
2025-01-16 17:25:49 +0700

    Create activity Login

---
+++ app/src/main/java/com/example/bookmoth/Login.java
@@ -0,0 +1,53 @@
+package com.example.bookmoth;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Login extends AppCompatActivity {
+
+    private Button login;
+    private FirebaseDatabase database;
+    private GoogleSignInOptions signInOptions;
+    private GoogleSignInClient client;
+    private FirebaseAuth firebaseAuth;
+
+    int RC_LOGIN = 20;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setUp();
+    }
+
+    private void setUp() {
+        login = findViewById(R.id.button);
+        database =FirebaseDatabase.getInstance();
+
+        signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.WEB_CLIENT_ID))
+                .requestEmail()
+                .build();
+
+        
+    }
+}
