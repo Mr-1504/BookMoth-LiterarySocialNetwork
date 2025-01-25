9575f70fba60f28837826bd9207162d7e6bca22b
Mr-1504 <truongvan.minh1504@gmail.com>
2025-01-25 21:46:40 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/bookmoth/MainActivity.java
+++ app/src/main/java/com/example/bookmoth/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.bookmoth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button but = findViewById(R.id.but);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PayActivity.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
