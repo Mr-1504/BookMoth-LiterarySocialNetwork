d540566658fba31df8014463c360c1429500c664
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-09 13:20:33 +0700

    Update TypeGenderActivity.java

--- app/src/main/java/com/example/bookmoth/ui/register/TypeGenderActivity.java
+++ app/src/main/java/com/example/bookmoth/ui/register/TypeGenderActivity.java
@@ -1,16 +1,33 @@
 package com.example.bookmoth.ui.register;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.bookmoth.R;
+import com.example.bookmoth.core.utils.GenderUtils;
+import com.example.bookmoth.domain.model.Gender;
+import com.example.bookmoth.ui.login.LoginActivity;
+import com.example.bookmoth.ui.viewmodel.registerViewModel.GenderViewModel;
 
 public class TypeGenderActivity extends AppCompatActivity {
+
+    private Button returnButton, iHaveAAccountButton, nextButton;
+    private TextView warning;
+    private RadioGroup gender;
+    private GenderViewModel genderViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +39,57 @@ public class TypeGenderActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        returnButton = findViewById(R.id.return_button);
+        iHaveAAccountButton = findViewById(R.id.i_have_a_account);
+        nextButton = findViewById(R.id.next_for_register);
+        warning = findViewById(R.id.warning);
+        gender = findViewById(R.id.gender_group);
+
+        genderViewModel = new ViewModelProvider(this).get(GenderViewModel.class);
+
+        clickNext();
+        clickIHaveAAccount();
+        clickReturn();
     }
+
+    private void clickReturn() {
+        returnButton.setOnClickListener(view -> finish());
+    }
+
+    private void clickIHaveAAccount() {
+        iHaveAAccountButton.setOnClickListener(view -> {
+            Intent intent = new Intent(TypeGenderActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+    }
+
+    private void clickNext() {
+        nextButton.setOnClickListener(view -> {
+            int selectedId = gender.getCheckedRadioButtonId();
+            if (selectedId == -1) {
+                warning.setVisibility(View.VISIBLE);
+            } else {
+                warning.setVisibility(View.GONE);
+                Gender selectedGender = getSelectedGender(selectedId);
+                genderViewModel.setGender(selectedGender);
+                Log.d("GENDER_SELECTED", "Selected gender: " + selectedGender); // Kiểm tra xem có gọi không
+                Intent intent = new Intent(TypeGenderActivity.this, TypeOtpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    private Gender getSelectedGender(int selectedId) {
+        if (selectedId == R.id.radio_male) {
+            return Gender.MALE;
+        } else if (selectedId == R.id.radio_female) {
+            return Gender.FEMALE;
+        } else {
+            return Gender.OTHER;
+        }
+    }
+
 }
