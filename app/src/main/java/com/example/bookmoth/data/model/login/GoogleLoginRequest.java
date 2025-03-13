24f9968b365b074d9c5d79e8cc1b2cdfa0335267
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-13 23:19:23 +0700

    Add authenticator

---
+++ app/src/main/java/com/example/bookmoth/data/model/login/GoogleLoginRequest.java
@@ -0,0 +1,9 @@
+package com.example.bookmoth.data.model.login;
+
+public class GoogleLoginRequest {
+    private String idToken;
+
+    public GoogleLoginRequest(String idToken) {
+        this.idToken = idToken;
+    }
+}
