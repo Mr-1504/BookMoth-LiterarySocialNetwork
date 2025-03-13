ea14c6d3c1ef7178bfe8cf4e0c67d074058508d1
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-13 23:20:48 +0700

    Update Account.java

--- app/src/main/java/com/example/bookmoth/domain/model/login/Account.java
+++ app/src/main/java/com/example/bookmoth/domain/model/login/Account.java
@@ -3,14 +3,10 @@ package com.example.bookmoth.domain.model.login;
 public class Account {
     private int accountId;
     private String email;
-    private String password;
-    private int accountType;
 
-    public Account(int accountId, String email, String password, int accountType) {
+    public Account(int accountId, String email) {
         this.accountId = accountId;
         this.email = email;
-        this.password = password;
-        this.accountType = accountType;
     }
 
     public int getAccountId() {
@@ -27,21 +23,5 @@ public class Account {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(int accountType) {
-        this.accountType = accountType;
     }
 }
