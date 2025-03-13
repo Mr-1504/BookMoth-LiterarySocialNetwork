05828bde7a73d637bf1c5f599d83015373ba2f76
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-13 23:20:14 +0700

    Update LoginUseCase.java

--- app/src/main/java/com/example/bookmoth/domain/usecase/login/LoginUseCase.java
+++ app/src/main/java/com/example/bookmoth/domain/usecase/login/LoginUseCase.java
@@ -4,18 +4,24 @@ import com.example.bookmoth.domain.model.login.Account;
 import com.example.bookmoth.domain.model.login.Token;
 import com.example.bookmoth.domain.repository.login.LoginRepository;
 
-import java.io.IOException;
-
 import retrofit2.Call;
 
 public class LoginUseCase {
-    private LoginRepository authRepository;
+    private final LoginRepository authRepository;
 
     public LoginUseCase(LoginRepository authRepository){
         this.authRepository = authRepository;
     }
 
-    public Token login (String email, String password) throws IOException {
-        return authRepository.login(email, password);
+    public Call<Token> login (String email, String password){
+        return  authRepository.login(email, password);
+    }
+
+    public Call<Token> googleLogin(String idToken){
+        return authRepository.googleLogin(idToken);
+    }
+
+    public Call<Account> getAccount(){
+        return authRepository.getAccount();
     }
 }
