5342a7e83a1f28b82c0816b653a03772156647ba
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-13 23:20:31 +0700

    Update RegisterViewModel.java

--- app/src/main/java/com/example/bookmoth/ui/viewmodel/registerViewModel/RegisterViewModel.java
+++ app/src/main/java/com/example/bookmoth/ui/viewmodel/registerViewModel/RegisterViewModel.java
@@ -94,107 +94,107 @@ public class RegisterViewModel extends ViewModel implements Serializable {
     }
 
     public void checkEmailExists(Context context, RegisterUseCase useCase, final OnCheckEmailExistsListener listener) {
-        useCase.checkEmailExistsExecute(this.getEmail()).enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                int statusCode = response.code();
-
-                if (statusCode == 200) {
-                    // Email đã tồn tại (status code 200 OK)
-                    Log.d("EmailCheck", "Email đã tồn tại");
-                    listener.onError(context.getString(R.string.email_already_exists));
-                } else if (statusCode == 204) {
-                    // Email chưa tồn tại (status code 204 No Content)
-                    Log.d("EmailCheck", "Email chưa tồn tại");
-                    listener.onSuccess();
-                } else {
-                    // Xử lý các status code khác nếu có
-                    Log.d("EmailCheck", "Mã trạng thái không mong đợi: " + statusCode);
-                    listener.onError(context.getString(R.string.undefined_error));
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                listener.onError(context.getString(R.string.error_connecting_to_server));
-            }
-        });
+//        useCase.checkEmailExistsExecute(this.getEmail()).enqueue(new Callback<Void>() {
+//            @Override
+//            public void onResponse(Call<Void> call, Response<Void> response) {
+//                int statusCode = response.code();
+//
+//                if (statusCode == 200) {
+//                    // Email đã tồn tại (status code 200 OK)
+//                    Log.d("EmailCheck", "Email đã tồn tại");
+//                    listener.onError(context.getString(R.string.email_already_exists));
+//                } else if (statusCode == 204) {
+//                    // Email chưa tồn tại (status code 204 No Content)
+//                    Log.d("EmailCheck", "Email chưa tồn tại");
+//                    listener.onSuccess();
+//                } else {
+//                    // Xử lý các status code khác nếu có
+//                    Log.d("EmailCheck", "Mã trạng thái không mong đợi: " + statusCode);
+//                    listener.onError(context.getString(R.string.undefined_error));
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Void> call, Throwable t) {
+//                listener.onError(context.getString(R.string.error_connecting_to_server));
+//            }
+//        });
     }
 
     public void getOtp(Context context, RegisterUseCase registerUseCase, final OnGetOtpListener listener) {
-        registerUseCase.getOtpExecute(
-                this.getEmail(),
-                this.getFirstName()
-        ).enqueue(new Callback<Otp>() {
-            @Override
-            public void onResponse(Call<Otp> call, Response<Otp> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    listener.onSuccess();
-                } else if (response.code() == 409) {
-                    listener.onError(context.getString(R.string.email_already_exists));
-                } else if (response.code() == 400) {
-                    listener.onError(context.getString(R.string.invalid_email)); // Sửa lỗi ở đây
-                } else if (response.code() == 422) {
-                    listener.onError(context.getString(R.string.cannot_send_otp)); // Sửa lỗi ở đây
-                } else {
-                    listener.onError(context.getString(R.string.undefined_error) + " " + response.code()); // Sửa lỗi ở đây
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Otp> call, Throwable t) {
-                listener.onError(context.getString(R.string.error_connecting_to_server) + " " + t.getMessage()); // Sửa lỗi ở đây
-            }
-        });
+//        registerUseCase.getOtpExecute(
+//                this.getEmail(),
+//                this.getFirstName()
+//        ).enqueue(new Callback<Otp>() {
+//            @Override
+//            public void onResponse(Call<Otp> call, Response<Otp> response) {
+//                if (response.isSuccessful() && response.body() != null) {
+//                    listener.onSuccess();
+//                } else if (response.code() == 409) {
+//                    listener.onError(context.getString(R.string.email_already_exists));
+//                } else if (response.code() == 400) {
+//                    listener.onError(context.getString(R.string.invalid_email)); // Sửa lỗi ở đây
+//                } else if (response.code() == 422) {
+//                    listener.onError(context.getString(R.string.cannot_send_otp)); // Sửa lỗi ở đây
+//                } else {
+//                    listener.onError(context.getString(R.string.undefined_error) + " " + response.code()); // Sửa lỗi ở đây
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Otp> call, Throwable t) {
+//                listener.onError(context.getString(R.string.error_connecting_to_server) + " " + t.getMessage()); // Sửa lỗi ở đây
+//            }
+//        });
     }
 
     public void verifyOtp(Context context, RegisterUseCase registerUseCase, final OnVerifyOtpListener listener) {
-        registerUseCase.verifyOtpExecute(this.getEmail(), this.getOtp()).enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                if (response.isSuccessful()) {
-                    listener.onSuccess();
-                } else {
-                    listener.onError(context.getString(R.string.invalid_otp));
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                listener.onError(context.getString(R.string.error_connecting_to_server));
-            }
-        });
+//        registerUseCase.verifyOtpExecute(this.getEmail(), this.getOtp()).enqueue(new Callback<Void>() {
+//            @Override
+//            public void onResponse(Call<Void> call, Response<Void> response) {
+//                if (response.isSuccessful()) {
+//                    listener.onSuccess();
+//                } else {
+//                    listener.onError(context.getString(R.string.invalid_otp));
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Void> call, Throwable t) {
+//                listener.onError(context.getString(R.string.error_connecting_to_server));
+//            }
+//        });
     }
 
     public void register(Context context, RegisterUseCase registerUseCase, final OnRegisterListener listener) {
-        registerUseCase.registerExecute(
-                this.getFirstName(),
-                this.getLastName(),
-                this.getEmail(),
-                this.getPassword(),
-                GenderUtils.getGenderIntValue(this.getGender()),
-                this.getAccountType()
-        ).enqueue(new Callback<Token>() {
-            @Override
-            public void onResponse(Call<Token> call, Response<Token> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    listener.onSuccess();
-                } else if (response.code() == 400) {
-                    listener.onError(context.getString(R.string.invalid_email));
-                } else if (response.code() == 409) {
-                    listener.onError(context.getString(R.string.email_already_exists));
-                } else if (response.code() == 422) {
-                    listener.onError(context.getString(R.string.cannot_register));
-                } else {
-                    listener.onError(context.getString(R.string.undefined_error) + " " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Token> call, Throwable t) {
-                listener.onError(context.getString(R.string.error_connecting_to_server));
-            }
-        });
+//        registerUseCase.registerExecute(
+//                this.getFirstName(),
+//                this.getLastName(),
+//                this.getEmail(),
+//                this.getPassword(),
+//                GenderUtils.getGenderIntValue(this.getGender()),
+//                this.getAccountType()
+//        ).enqueue(new Callback<Token>() {
+//            @Override
+//            public void onResponse(Call<Token> call, Response<Token> response) {
+//                if (response.isSuccessful() && response.body() != null) {
+//                    listener.onSuccess();
+//                } else if (response.code() == 400) {
+//                    listener.onError(context.getString(R.string.invalid_email));
+//                } else if (response.code() == 409) {
+//                    listener.onError(context.getString(R.string.email_already_exists));
+//                } else if (response.code() == 422) {
+//                    listener.onError(context.getString(R.string.cannot_register));
+//                } else {
+//                    listener.onError(context.getString(R.string.undefined_error) + " " + response.code());
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<Token> call, Throwable t) {
+//                listener.onError(context.getString(R.string.error_connecting_to_server));
+//            }
+//        });
     }
 
     public interface OnGetOtpListener {
