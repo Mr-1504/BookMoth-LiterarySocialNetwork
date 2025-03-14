544eba45d1ea13dced8b4844d83838a9837c29fd
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-14 16:31:24 +0700

    Update RegisterRepositoryImpl.java

--- app/src/main/java/com/example/bookmoth/data/repository/register/RegisterRepositoryImpl.java
+++ app/src/main/java/com/example/bookmoth/data/repository/register/RegisterRepositoryImpl.java
@@ -1,16 +1,18 @@
 package com.example.bookmoth.data.repository.register;
 
-import com.example.bookmoth.core.utils.Result;
 import com.example.bookmoth.data.model.register.GetOtpRequest;
+import com.example.bookmoth.data.model.register.GoogleRegisterRequest;
+import com.example.bookmoth.data.model.register.RegisterRequest;
+import com.example.bookmoth.data.model.register.TokenResponse;
+import com.example.bookmoth.data.model.register.VerifyOtpRequest;
 import com.example.bookmoth.data.remote.utils.RetrofitClient;
 import com.example.bookmoth.data.remote.register.RegisterApiService;
 import com.example.bookmoth.domain.model.login.Token;
 import com.example.bookmoth.domain.model.register.Otp;
 import com.example.bookmoth.domain.repository.register.RegisterRepository;
 
-import java.io.IOException;
+import retrofit2.Call;
 
-import retrofit2.Response;
 
 public class RegisterRepositoryImpl implements RegisterRepository {
     private final RegisterApiService registerApiService;
@@ -19,36 +21,29 @@ public class RegisterRepositoryImpl implements RegisterRepository {
         this.registerApiService = RetrofitClient.getInstance().create(RegisterApiService.class);
     }
 
+
     @Override
-    public Result<Otp> getOtp(String email, String name) throws IOException {
-        Response<Otp> response = registerApiService.getOtp(new GetOtpRequest(email, name)).execute();
-        if (response.isSuccessful() && response.body() != null) {
-            return new Result.Success<>(response.body());
-        } else {
-            throw new IOException("Error connecting to server");
-        }
+    public Call<Otp> getOtp(String email, String name) {
+        return registerApiService.getOtp(new GetOtpRequest(email, name));
     }
 
     @Override
-    public Result<Void> checkEmailExists(String email) {
-        return new Result.Success<>(null);
+    public Call<Void> checkEmailExists(String email) {
+        return registerApiService.checkEmailExists(email);
     }
 
     @Override
-    public Result<Void> verifyOtp(String email, String otp) {
-        return null;
+    public Call<Void> verifyOtp(String email, String otp) {
+        return registerApiService.verifyOtp(new VerifyOtpRequest(email, otp));
     }
 
     @Override
-    public Result<Token> register(
-            String firstName,
-            String lastName,
-            String email,
-            String password,
-            int gender,
-            int accountType) {
-        return new Result.Success<>(null);
+    public Call<Token> register(String firstName, String lastName, String email, String password, int gender) {
+        return registerApiService.register(new RegisterRequest(firstName, lastName, email, password, gender));
     }
 
-
+    @Override
+    public Call<TokenResponse> googleRegister(String idToken) {
+        return registerApiService.googleRegister(new GoogleRegisterRequest(idToken));
+    }
 }
