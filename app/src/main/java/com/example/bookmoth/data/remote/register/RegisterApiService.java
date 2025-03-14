7ecd9a2361980381a23921b8ce53d8bae54bb996
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-14 16:31:21 +0700

    Update RegisterApiService.java

--- app/src/main/java/com/example/bookmoth/data/remote/register/RegisterApiService.java
+++ app/src/main/java/com/example/bookmoth/data/remote/register/RegisterApiService.java
@@ -1,7 +1,9 @@
 package com.example.bookmoth.data.remote.register;
 
 import com.example.bookmoth.data.model.register.GetOtpRequest;
+import com.example.bookmoth.data.model.register.GoogleRegisterRequest;
 import com.example.bookmoth.data.model.register.RegisterRequest;
+import com.example.bookmoth.data.model.register.TokenResponse;
 import com.example.bookmoth.data.model.register.VerifyOtpRequest;
 import com.example.bookmoth.domain.model.login.Token;
 import com.example.bookmoth.domain.model.register.Otp;
@@ -24,4 +26,7 @@ public interface RegisterApiService {
 
     @POST("api/account/register")
     Call<Token> register(@Body RegisterRequest request);
+
+    @POST("api/account/auth/google-register")
+    Call<TokenResponse> googleRegister(@Body GoogleRegisterRequest request);
 }
