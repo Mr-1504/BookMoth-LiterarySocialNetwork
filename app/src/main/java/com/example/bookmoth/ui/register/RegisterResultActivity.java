1e1aeaba00c73ba750ebd3d3dd28fe9d47a9cd8d
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-14 16:31:46 +0700

    Update RegisterResultActivity.java

--- app/src/main/java/com/example/bookmoth/ui/register/RegisterResultActivity.java
+++ app/src/main/java/com/example/bookmoth/ui/register/RegisterResultActivity.java
@@ -1,6 +1,9 @@
 package com.example.bookmoth.ui.register;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +11,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
 import com.example.bookmoth.R;
+import com.example.bookmoth.data.repository.profile.ProfileRepositoryImpl;
+import com.example.bookmoth.domain.model.profile.Profile;
+import com.example.bookmoth.domain.usecase.profile.ProfileUseCase;
+import com.example.bookmoth.ui.viewmodel.ProfileViewModel;
 
 public class RegisterResultActivity extends AppCompatActivity {
+
+    private ImageView avatar;
+    private TextView tvWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +33,29 @@ public class RegisterResultActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        avatar = findViewById(R.id.imgAvatar);
+        tvWelcome = findViewById(R.id.tvWelcome);
+
+        ProfileViewModel profileViewModel = new ProfileViewModel(new ProfileUseCase(new ProfileRepositoryImpl()));
+
+        profileViewModel.getProfile(this, new ProfileViewModel.OnProfileListener() {
+            @Override
+            public void onProfileSuccess(Profile profile) {
+                tvWelcome.setText(profile.getFirstName() + " " + getString(R.string.welcom_to_bookmoth));
+
+                Glide.with(RegisterResultActivity.this)
+                        .load(profile.getAvatar())
+                        .placeholder(R.drawable.avatar)
+                        .into(avatar);
+            }
+
+            @Override
+            public void onProfileFailure(String error) {
+                Toast.makeText(RegisterResultActivity.this, error, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
