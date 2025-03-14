4c8c8dbf1115515aed82c0624357d92c0cbfd285
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-14 16:31:38 +0700

    Update RegisterUseCase.java

--- app/src/main/java/com/example/bookmoth/domain/usecase/register/RegisterUseCase.java
+++ app/src/main/java/com/example/bookmoth/domain/usecase/register/RegisterUseCase.java
@@ -1,11 +1,10 @@
 package com.example.bookmoth.domain.usecase.register;
 
-import com.example.bookmoth.core.utils.Result;
+import com.example.bookmoth.data.model.register.TokenResponse;
 import com.example.bookmoth.domain.model.login.Token;
 import com.example.bookmoth.domain.model.register.Otp;
 import com.example.bookmoth.domain.repository.register.RegisterRepository;
 
-import java.io.IOException;
 
 import retrofit2.Call;
 
@@ -16,26 +15,29 @@ public class RegisterUseCase {
         this.registerRepository = registerRepository;
     }
 
-    public Result<Otp> getOtpExecute(String email, String name) throws IOException {
+    public Call<Otp> getOtpExecute(String email, String name) {
         return registerRepository.getOtp(email, name);
     }
 
-    public Result<Void> checkEmailExistsExecute(String email) {
+    public Call<Void> checkEmailExistsExecute(String email) {
         return registerRepository.checkEmailExists(email);
     }
 
-    public Result<Void> verifyOtpExecute(String email, String otp) {
+    public Call<Void> verifyOtpExecute(String email, String otp) {
         return registerRepository.verifyOtp(email, otp);
     }
 
-    public Result<Token> registerExecute(
+    public Call<Token> registerExecute(
             String firstName,
             String lastName,
             String email,
             String password,
-            int gender,
-            int accountType
+            int gender
     ) {
-        return registerRepository.register(firstName, lastName, email, password, gender, accountType);
+        return registerRepository.register(firstName, lastName, email, password, gender);
+    }
+
+    public Call<TokenResponse> registerGoogleExecute(String idToken) {
+        return registerRepository.googleRegister(idToken);
     }
 }
