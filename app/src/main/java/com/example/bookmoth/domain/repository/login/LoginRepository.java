26090a84ac3e69e7c01519ff4e405f9628b91bbd
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-17 17:00:48 +0700

    Change from Token to TokenResponse

--- app/src/main/java/com/example/bookmoth/domain/repository/login/LoginRepository.java
+++ app/src/main/java/com/example/bookmoth/domain/repository/login/LoginRepository.java
@@ -1,15 +1,15 @@
 package com.example.bookmoth.domain.repository.login;
 
+import com.example.bookmoth.data.model.register.TokenResponse;
 import com.example.bookmoth.domain.model.login.Account;
-import com.example.bookmoth.domain.model.login.Token;
 
 
 import retrofit2.Call;
 
 public interface LoginRepository {
-    Call<Token> login(String email, String password);
+    Call<TokenResponse> login(String email, String password);
 
-    Call<Token> googleLogin(String idToken);
+    Call<TokenResponse> googleLogin(String idToken);
 
     Call<Account> getAccount();
 }
