476e8f4edb83b689c8f9a82a217116f5f71880ab
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-17 16:58:48 +0700

    Add birth attribute

--- app/src/main/java/com/example/bookmoth/data/model/register/RegisterRequest.java
+++ app/src/main/java/com/example/bookmoth/data/model/register/RegisterRequest.java
@@ -6,19 +6,21 @@ public class RegisterRequest {
     private String email;
     private String password;
     private int gender;
-    private int accountType;
+    private String dateOfBirth;
 
     public RegisterRequest(
             String firstName,
             String lastName,
             String email,
             String password,
-            int gender
+            int gender,
+            String dateOfBirth
     ) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
     }
 }
