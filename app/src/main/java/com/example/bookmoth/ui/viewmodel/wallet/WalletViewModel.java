2cdcb07ea33e86ee8334851255fcaddc50821c10
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-26 13:54:49 +0700

    Update UI for deposit

--- app/src/main/java/com/example/bookmoth/ui/viewmodel/wallet/WalletViewModel.java
+++ app/src/main/java/com/example/bookmoth/ui/viewmodel/wallet/WalletViewModel.java
@@ -18,36 +18,126 @@ public class WalletViewModel {
 
     /**
      * Khởi tạo WalletViewModel
+     *
      * @param walletUseCase usecase chứa các phương thức liên quan đến ví
      */
-    public WalletViewModel(WalletUseCase walletUseCase){
+    public WalletViewModel(WalletUseCase walletUseCase) {
         this.walletUseCase = walletUseCase;
     }
 
-    public void getBalance(Context context, OnWalletListener listener){
-       walletUseCase.getBalance().enqueue(new Callback<BalanceResponse>() {
-           @Override
-           public void onResponse(Call<BalanceResponse> call, Response<BalanceResponse> response) {
-               if (response.isSuccessful() && response.body() != null){
-                   listener.onGetBalanceSuccess(response.body());
-               } else if (response.code() == 401){
-                   listener.onGetBalanceFailed(context.getString(R.string.error_invalid_account));
-               } else if (response.code() == 404) {
-                   listener.onGetBalanceFailed(context.getString(R.string.account_does_not_exist));
-               } else {
-                     listener.onGetBalanceFailed(context.getString(R.string.undefined_error));
-               }
-           }
+    /**
+     * Lấy số dư của ví
+     *
+     * @param context  context của activity
+     * @param listener listener lắng nghe kết quả
+     */
+    public void getBalance(Context context, OnWalletListener listener) {
+        walletUseCase.getBalance().enqueue(new Callback<BalanceResponse>() {
+            @Override
+            public void onResponse(Call<BalanceResponse> call, Response<BalanceResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    listener.onSuccess(response.body());
+                } else if (response.code() == 401) {
+                    listener.onFailed(context.getString(R.string.error_invalid_account));
+                } else if (response.code() == 404) {
+                    listener.onFailed(context.getString(R.string.account_does_not_exist));
+                } else {
+                    listener.onFailed(context.getString(R.string.undefined_error));
+                }
+            }
 
-           @Override
-           public void onFailure(Call<BalanceResponse> call, Throwable t) {
-                listener.onGetBalanceFailed(context.getString(R.string.error_connecting_to_server));
-           }
-       });
+            @Override
+            public void onFailure(Call<BalanceResponse> call, Throwable t) {
+                listener.onFailed(context.getString(R.string.error_connecting_to_server));
+            }
+        });
     }
 
-    public interface OnWalletListener{
-        void onGetBalanceSuccess(BalanceResponse balanceResponse);
-        void onGetBalanceFailed(String error);
+    public void createWallet(Context context, String pin, OnWalletListener listener) {
+        walletUseCase.createWallet(pin).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    listener.onSuccess(null);
+                } else if (response.code() == 400) {
+                    listener.onFailed(context.getString(R.string.invalid_data));
+                } else if (response.code() == 401) {
+                    listener.onFailed(context.getString(R.string.error_invalid_account));
+                } else if (response.code() == 409) {
+                    listener.onFailed(context.getString(R.string.wallet_already_exists));
+                } else if (response.code() == 422){
+                    listener.onFailed(context.getString(R.string.cannot_process_request));
+                } else {
+                    listener.onFailed(context.getString(R.string.undefined_error));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                listener.onFailed(context.getString(R.string.error_connecting_to_server));
+            }
+        });
     }
+
+    public void confirmPin(Context context, String pin, OnWalletListener listener) {
+        walletUseCase.confirmPin(pin).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    listener.onSuccess(null);
+                } else if (response.code() == 400) {
+                    listener.onFailed(context.getString(R.string.invalid_data));
+                } else if (response.code() == 401) {
+                    listener.onFailed(context.getString(R.string.error_invalid_account));
+                } else if (response.code() == 404) {
+                    listener.onFailed(context.getString(R.string.wallet_does_not_exist));
+                } else {
+                    listener.onFailed(context.getString(R.string.undefined_error));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                listener.onFailed(context.getString(R.string.error_connecting_to_server));
+            }
+        });
+    }
+
+    /**
+     * Kiểm tra ví đã tồn tại chưa
+     *
+     * @param context  context của activity
+     * @param listener listener lắng nghe kết quả
+     */
+    public void checkWalletExist(Context context, OnWalletListener listener) {
+        walletUseCase.checkWalletExist().enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    listener.onSuccess(null);
+                } else if (response.code() == 400) {
+                    listener.onFailed(context.getString(R.string.invalid_data));
+                } else if (response.code() == 404) {
+                    listener.onFailed(context.getString(R.string.wallet_does_not_exist));
+                } else {
+                    listener.onFailed(context.getString(R.string.undefined_error));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                listener.onFailed(context.getString(R.string.error_connecting_to_server));
+            }
+        });
+    }
+
+    /**
+     * Interface lắng nghe kết quả
+     */
+    public interface OnWalletListener {
+        void onSuccess(BalanceResponse balanceResponse);
+        void onFailed(String error);
+    }
+
+
 }
