80d78e807acf9608eefdc6231e6d86c6fb42b3e0
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-26 23:33:34 +0700

    Update layout

--- app/src/main/java/com/example/bookmoth/data/remote/utils/AuthInterceptor.java
+++ app/src/main/java/com/example/bookmoth/data/remote/utils/AuthInterceptor.java
@@ -33,4 +33,3 @@ public class AuthInterceptor implements Interceptor {
         return chain.proceed(request);
     }
 }
-
---
+++ app/src/main/java/com/example/bookmoth/ui/dialogs/LoadingDialog.java
@@ -0,0 +1,37 @@
+package com.example.bookmoth.ui.dialogs;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.bookmoth.R;
+
+public class LoadingDialog extends DialogFragment {
+    public static LoadingDialog newInstance() {
+        return new LoadingDialog();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_loading, container, false);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (getDialog() != null) {
+            getDialog().getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+    }
+}
+
