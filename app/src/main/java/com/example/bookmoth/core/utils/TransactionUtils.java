c8843062eefe7c41b01720d7e820b229ddf057cc
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-30 21:31:31 +0700

    Update TransactionUtils.java

--- app/src/main/java/com/example/bookmoth/core/utils/TransactionUtils.java
+++ app/src/main/java/com/example/bookmoth/core/utils/TransactionUtils.java
@@ -4,14 +4,14 @@ package com.example.bookmoth.core.utils;
 import com.example.bookmoth.domain.model.payment.TransactionType;
 
 public class TransactionUtils {
-    public static boolean getTransactionType(TransactionType type) {
+    public static int getTransactionType(TransactionType type) {
         switch (type) {
             case DEPOSIT:
-                return true;
+                return 1;
             case WITHDRAWAL:
-                return false;
+                return 2;
             default:
-                return true;
+                return 1;
         }
     }
 }
