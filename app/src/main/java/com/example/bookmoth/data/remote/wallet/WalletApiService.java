acf2171b3b8b2005aac39c6d1858250892a2bec7
Mr-1504 <truongvan.minh1504@gmail.com>
2025-04-01 00:15:53 +0700

    Update WalletApiService.java

--- app/src/main/java/com/example/bookmoth/data/remote/wallet/WalletApiService.java
+++ app/src/main/java/com/example/bookmoth/data/remote/wallet/WalletApiService.java
@@ -6,11 +6,16 @@ import com.example.bookmoth.data.model.payment.ZaloPayTokenResponse;
 import com.example.bookmoth.data.model.wallet.ConfirmPinRequest;
 import com.example.bookmoth.data.model.wallet.CreateWalletRequest;
 import com.example.bookmoth.data.model.wallet.OrderProductRequest;
+import com.example.bookmoth.data.model.wallet.PaymentRequest;
+import com.example.bookmoth.data.model.wallet.UpdatePaymentMethodRequest;
+import com.example.bookmoth.data.model.wallet.ZaloPayOrderRequest;
 import com.example.bookmoth.domain.model.wallet.BalanceResponse;
+import com.example.bookmoth.domain.model.wallet.OrderWorkResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 
 /**
@@ -66,5 +71,30 @@ public interface WalletApiService {
      * @return {@link Call} chứa {@link String}, mã giao dịch.
      */
     @POST("api/wallet/order")
-    Call<String> orderProduct(@Body OrderProductRequest request);
+    Call<OrderWorkResponse> orderProduct(@Body OrderProductRequest request);
+
+    /**
+     * Cập nhật phương thức thanh toán
+     * @param request thông tin cập nhật
+     * @return kết quả cập nhật
+     */
+    @PATCH("api/wallet/payment-method")
+    Call<Void> updatePaymentMethod(@Body UpdatePaymentMethodRequest request);
+
+    /**
+     * Thanh toán
+     * @param request thông tin thanh toán
+     * @return kết quả thanh toán
+     */
+    @POST("api/wallet/payment")
+    Call<Void> payment(@Body PaymentRequest request);
+
+    /**
+     * Tạo đơn hàng để nạp tiền qua ZaloPay.
+     *
+     * @param request Đối tượng {@link ZaloPayOrderRequest} chứa thông tin giao dịch.
+     * @return {@link Call} chứa {@link ZaloPayTokenResponse}, phản hồi từ ZaloPay.
+     */
+    @POST("api/wallet/zalopay-order")
+    Call<ZaloPayTokenResponse> createZaloPayOrder(@Body ZaloPayOrderRequest request);
 }
