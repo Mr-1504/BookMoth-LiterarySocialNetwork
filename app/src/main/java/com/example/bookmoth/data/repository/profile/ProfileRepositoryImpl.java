2e445ea42d9f2457688f3fb9d741eee09ad34ee0
Mr-1504 <truongvan.minh1504@gmail.com>
2025-04-02 20:27:40 +0700

    Update ProfileRepositoryImpl.java

--- app/src/main/java/com/example/bookmoth/data/repository/profile/ProfileRepositoryImpl.java
+++ app/src/main/java/com/example/bookmoth/data/repository/profile/ProfileRepositoryImpl.java
@@ -3,9 +3,11 @@ package com.example.bookmoth.data.repository.profile;
 import com.example.bookmoth.data.remote.profile.ProfileApiService;
 import com.example.bookmoth.data.remote.utils.RetrofitClient;
 import com.example.bookmoth.domain.model.profile.Profile;
+import com.example.bookmoth.domain.model.profile.ProfileResponse;
 import com.example.bookmoth.domain.model.profile.UsernameResponse;
 import com.example.bookmoth.domain.repository.profile.ProfileRepository;
 
+import java.util.List;
 import java.util.Map;
 
 import okhttp3.MultipartBody;
@@ -57,9 +59,49 @@ public class ProfileRepositoryImpl implements ProfileRepository {
         return profileApiService.checkUsername(username);
     }
 
-
+    /**
+     * Chỉnh sửa thông tin hồ sơ người dùng.
+     *
+     * @param params thông tin hồ sơ cần chỉnh sửa.
+     * @param avatar ảnh đại diện mới.
+     * @param cover  ảnh bìa mới.
+     * @return {@code Call<Profile>} phản hồi chứa thông tin hồ sơ người dùng sau khi chỉnh sửa.
+     */
     @Override
     public Call<Profile> editProfile(Map<String, RequestBody> params,  MultipartBody.Part avatar, MultipartBody.Part cover) {
         return profileApiService.editProfile(params, avatar, cover);
+    }
+
+    /**
+     * Tìm kiếm hồ sơ người dùng theo tên.
+     *
+     * @param searchString tên cần tìm kiếm.
+     * @return {@code Call<List<ProfileResponse>} phản hồi chứa danh sách hồ sơ người dùng tìm được.
+     */
+    @Override
+    public Call<List<ProfileResponse>> searchProfile(String searchString) {
+        return profileApiService.searchProfile(searchString);
+    }
+
+    /**
+     * Follow người dùng.
+     *
+     * @param profileId id của người dùng cần follow
+     * @return {@code Call<Void>} phản hồi chứa thông tin hồ sơ người dùng sau khi follow.
+     */
+    @Override
+    public Call<Void> follow(String profileId) {
+        return profileApiService.follow(profileId);
+    }
+
+    /**
+     * Unfollow người dùng.
+     *
+     * @param profileId id của người dùng cần unfollow
+     * @return {@code Call<Void>} phản hồi chứa thông tin hồ sơ người dùng sau khi unfollow.
+     */
+    @Override
+    public Call<Void> unfollow(String profileId) {
+        return profileApiService.unfollow(profileId);
     }
 }
