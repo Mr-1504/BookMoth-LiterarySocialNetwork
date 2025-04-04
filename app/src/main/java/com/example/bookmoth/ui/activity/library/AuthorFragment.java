9eda63d42c1e2ab5bdfb092fcb177d75e69151d8
watashiwalds <watashiwalds@users.noreply.github.com>
2025-04-04 13:11:41 +0700

    things

--- app/src/main/java/com/example/bookmoth/ui/activity/library/AuthorFragment.java
+++ app/src/main/java/com/example/bookmoth/ui/activity/library/AuthorFragment.java
@@ -75,7 +75,7 @@ public class AuthorFragment extends Fragment {
         });
         rv_works_adapter.attachWorkClickListener(pos -> {
             Intent workDash = new Intent(requireContext(), WorkDashboardActivity.class);
-            workDash.putExtra("work_id", works.get(pos-1).getWork_id());
+            workDash.putExtra("work_id", works.get(pos-2).getWork_id());
             startActivity(workDash);
         });
         rv_works_adapter.attachRefreshListener(view -> viewModel.fetchCreatedWorks());
